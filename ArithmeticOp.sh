51b8c3e8a7be27d4d98f26fde3f98d06746435d9
sayalipawade <sayalipawade05@gmail.com>
2020-03-20 16:36:01 +0530

    Added:calculating the result of c+a/b

--- ArithmeticOp.sh
+++ ArithmeticOp.sh
@@ -5,9 +5,13 @@ read -p "Enter First No:" a
 read -p "Enter Second No:" b
 read -p "Enter Three No:" c
 
-#compute a+b*c
+#computing the result of a+b*c
 result=$((a+b*c))
 
-#compute a*b+c
+#computing the result of a*b+c
 result=$((a*b+c))
+
+#Computing the result of c+a/b
+result=$((c+a/b))
+
 
