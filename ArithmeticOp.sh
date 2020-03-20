cb623e1738d304d52103ca9164d2a23baed0601f
sayalipawade <sayalipawade05@gmail.com>
2020-03-20 18:24:58 +0530

    Added:Storing the result in dictionary for every computation

--- ArithmeticOp.sh
+++ ArithmeticOp.sh
@@ -1,18 +1,28 @@
 #!/bin/bash -x
-
+declare -A dictionary
 #Taking three inputs 
 read -p "Enter First No:" a
 read -p "Enter Second No:" b
 read -p "Enter Three No:" c
 
 #computing the result of a+b*c
-result=$((a+b*c))
+result1=$((a+b*c))
 
 #computing the result of a*b+c
-result=$((a*b+c))
+result2=$((a*b+c))
 
 #Computing the result of c+a/b
-result=$((c+a/b))
+result3=$((c+a/b))
 
 #Computing the result of a%b+c
-result=$((a%b+c))
+result4=$((a%b+c))
+
+#Storing the result in dictionary for every computation
+dictionary[1]=$result1
+dictionary[2]=$result2
+dictionary[3]=$result3
+dictionary[4]=$result4
+
+echo ${!dictionary[@]}
+echo ${dictionary[@]}
+	
